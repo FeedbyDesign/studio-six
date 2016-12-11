f0b858ee989ec128b7445b4a906f75734821808e
Marc Coet <marcoet@gmail.com>
2016-12-11 16:38:26 +0100

    react app initiated and ready for "Under Construction"

---
+++ src/UnderConstruction.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const UnderConstruction = () => {
+  return (
+    <div style={{color: "#F06", maxWidth: 300, margin: "auto", marginTop: 100, textAlign: "center"}}>
+      <h1>Studio Six</h1>
+      <p>
+        The website is under construction<br></br>
+        Please come back in a few days
+      </p>
+    </div>
+  )
+}
+
+export default UnderConstruction
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import App from './App';
+import UnderConstruction from './UnderConstruction';
+import './index.css';
+
+ReactDOM.render(
+  <UnderConstruction />,
+  document.getElementById('root')
+);
