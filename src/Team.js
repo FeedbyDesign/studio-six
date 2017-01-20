2141f78307b9a34e5ce1da4408ea5d5d71140d9c
Marc Coet <marcoet@gmail.com>
2017-01-20 17:43:43 +0100

    correction facebook link

--- src/Team.js
+++ src/Team.js
@@ -39,7 +39,7 @@ if (window) {
 const fbClick = () => {
   // setTimeout(function () { window.location = "https://www.facebook.com/223846264367486/"; }, 25);
   if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
-    window.location = "fb://page/223846264367486"
+    window.location = "fb://pages/223846264367486/"
     // window.location = "fb://page/223846264367486";
   }
   setTimeout(() => { window.open('https://www.facebook.com/223846264367486/', '_blank') }, 25);
